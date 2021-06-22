32f07ec3bba501068f64cd2c376ed75299eb7574
Cayir Aydar <aysegul.cayir-aydar@capgemini.com>
2021-06-22 14:14:55 +0200

    Initial commit

---
+++ restaurant/src/test/java/com/capgemini/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,18 @@
+package com.capgemini.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApplicationTests.class,args)
+	}
+
+	@Test
+	void contextLoads() {
+	}
+
+}
